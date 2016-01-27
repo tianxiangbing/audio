893bedb6676281ab2f551f0154f1564ea48b596f
tianxiangbing <55342775@qq.com>
2016-01-27 15:52:44 +0800

    发布

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,30 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const watch = require('gulp-watch');
 const uglify = require('gulp-uglify');
-gulp.task('default', () => {
+const cssmin = require('gulp-minify-css');
+gulp.task('js', () => {
     return gulp.src(["src/*.js"])
         .pipe(babel({
             presets: ['es2015'],
             // plugins: ['transform-runtime']
         }))
-        // .pipe(uglify())
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+gulp.task('css',()=>{
+    return gulp .src(["src/*.css"])
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist'));
+});
+gulp.task('img',()=>{
+    return gulp .src(["src/*.gif","src/*.png","src/*.jpg"])
         .pipe(gulp.dest('dist'));
 });
 gulp.task('w',()=>{
-	watch(["src/*.js"],()=>{
-		gulp.start('default');
-	})
-})
+    watch(["src/*.*"],()=>{
+    	gulp.start(['js','css','img']);
+    })
+});
+gulp.task('default',()=>{
+    gulp.start(['js','css','img']);
+});
